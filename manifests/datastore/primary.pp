e835a7dd6dd84aadb748e8e674d487024db95a15
John Bond <github@johnbond.org>
2018-08-16 14:54:33 +0100

    add postgress helpers

---
+++ manifests/datastore/primary.pp
@@ -0,0 +1,52 @@
+# @summary class to ensure postgress is configuered as the primary database
+#
+class wombat::datastore::primary (
+  Stdlib::IP::Adress::V4 $ipv4_address,
+  Stdlib::IP::Adress::V6 $ipv6_address,
+) {
+  assert_private()
+  include wombat::datastore
+
+  $archive_dir = $wombat::datastore::archive_dir
+  postgresql::server::pg_hba_rule { 'replication_v4':
+    ensure      => present,
+    address     => $ipv4_address,
+    auth_method => 'md5',
+    database    => 'replication',
+    order       => 5,
+    type        => 'host',
+    user        => 'wombat_replication',
+  }
+  postgresql::server::pg_hba_rule { 'replication_v4':
+    ensure      => present,
+    address     => $ipv6_address,
+    auth_method => 'md5',
+    database    => 'replication',
+    order       => 6,
+    type        => 'host',
+    user        => 'wombat_replication',
+  }
+  postgresql::server::config_entry { 'archive_command':
+    ensure => present,
+    value  => "test ! -f ${archive_dir}/%f && cp %p ${archive_dir}/%f",
+  }
+  postgresql::server::config_entry { 'archive_mode':
+    ensure => present,
+    value  => 'on',
+  }
+  postgresql::server::config_entry { 'max_wal_senders':
+    ensure => present,
+    value  => '3',
+  }
+  postgresql::server::config_entry { 'synchronous_standby_names':
+    ensure => present,
+    value  => 'FIRST 1 (lax)',
+  }
+  postgresql::server::config_entry { 'wal_level':
+    ensure => present,
+    value  => 'replica',
+  }
+  file {'/etc/postgresql/10/main/recovery.conf':
+    ensure => absent,
+  }
+}
---
+++ manifests/datastore/standby.pp
@@ -0,0 +1,26 @@
+# @summary class to ensure postgress is configuered as the standby database
+#
+class wombat::datastore::standby (
+  Stdlib::IP::Address $db_host,
+  Stdlib::IP::Address $db_username,
+  Stdlib::IP::Address $db_password,
+) {
+  assert_private()
+  include wombat::datastore
+
+  $archive_dir = $wombat::datastore::archive_dir
+  postgresql::server::pg_hba_rule { 'replication_v4': ensure => absent }
+  postgresql::server::pg_hba_rule { 'replication_v4': ensure => absent }
+  postgresql::server::config_entry { 'archive_command': ensure => absent }
+  postgresql::server::config_entry { 'archive_mode': ensure => absent }
+  postgresql::server::config_entry { 'max_wal_senders': ensure => absent }
+  postgresql::server::config_entry { 'synchronous_standby_names':
+    ensure => absent,
+  }
+  postgresql::server::config_entry { 'wal_level': ensure => absent }
+  file {'/etc/postgresql/10/main/recovery.conf':
+    ensure  => file,
+    content => template('wombat/etc/postgresql/10/main/recovery.conf.erb'),
+    notify  => Service[$postgresql::server::service_name],
+  }
+}
