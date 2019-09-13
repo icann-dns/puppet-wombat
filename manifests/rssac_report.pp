8cbb2c19e116f6565f7ecde0ce84e01dc3c40fbf
David Soltero <david.soltero@icann.org>
2019-09-13 09:16:26 -0700

    remove duplicate variables

--- manifests/rssac_report.pp
+++ manifests/rssac_report.pp
@@ -1,28 +1,24 @@
 # @summary installs the required files for the rssac publishing 
 #
 # @param packages packages to install
-# @param web_root location for the web_root
-# @param output_dir output dir for the rssac reports
 # @param report type of report to perform
 # @param server server name
 # @param report_server_name service FQDN
 #
 class wombat::rssac_report (
   Array[String[1]]                $packages,
-  Stdlib::Unixpath                $web_root,
-  String[1]                       $output_dir,
   String[1]                       $report,
   String[1]                       $server,
   Stdlib::Host                    $report_server_name,
 ) {
   ensure_packages($packages)
   include wombat::config
-  $_directories = [ $web_root , "${web_root}/${output_dir}" ]
+  $_directories = [ $::wombat::config::rssac_outdir ]
   ensure_resource(
     'file', $_directories, { 'ensure' => 'directory', mode => '0755' }
   )
   cron {'wombat-rssac-reports':
-    command => "/usr/bin/wombat-rssac-reports --output-dir ${output_dir} --report ${report} --server ${server} --report-server-name ${report_server_name}",
+    command => "/usr/bin/wombat-rssac-reports --output-dir ${::wombat::config::rssac_outdir} --report ${report} --server ${server} --report-server-name ${report_server_name}",
     user    => 'root',
     minute  => '0',
     hour    => '1',
