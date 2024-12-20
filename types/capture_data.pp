7b2d9e4a4a31359a083e842d3c63b304bfe7565a
jbond <github@johnbond.org>
2024-12-20 15:10:44 +0100

    fix style

--- types/capture_data.pp
+++ types/capture_data.pp
@@ -1,4 +1,5 @@
 # @summary Data types for capture data
 type Wombat::Capture_data = Enum['query-questions', 'query-answers',
   'query-authority', 'query-additional', 'response-questions', 'response-answers',
-  'response-authority', 'response-additional', 'query-all', 'response-all', 'all']
+  'response-authority', 'response-additional', 'query-all', 'response-all', 'all'
+]
